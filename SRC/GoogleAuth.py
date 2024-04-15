b8582c134d2660921beface041fca0431d7f71f0
harinik05 <hka747910@gmail.com>
2024-04-15 12:38:15 -0400

    commiting the pre-processing files

---
+++ SRC/GoogleAuth.py
@@ -0,0 +1,40 @@
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import os
+
+# Class - Allows us to authenticate from the google drive API 
+class GoogleDriveConnector:
+    SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+    # Pass the credentials.json file as the input (Google Developers Console, OAuth)
+    def __init__(self, credentials_file='credentials.json'):
+        self.credentials_file = credentials_file
+
+    # Authenticate 
+    def authenticate(self):
+        creds = None
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json')
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.credentials_file, self.SCOPES)
+                creds = flow.run_local_server(port=0)
+
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        return creds
+
+    # Access the google drive system after the token.json file is made
+    def mount_drive(self):
+        creds = self.authenticate()
+        service = build('drive', 'v3', credentials=creds)
+
+        root_folder_id = service.files().get(fileId='root').execute()['id']
+        os.system(f"google-drive-ocamlfuse -headless -id {root_folder_id}")
+        print("Mounted at /content/google_drive")
---
+++ SRC/PreProcessing.py
@@ -0,0 +1,29 @@
+import torch 
+import torchvision 
+import matplotlib.pyplot as plt
+from torchvision import transforms
+
+# Data pre-processing tasks
+class PreProcessing():
+    # Constructor
+    def __init__(self):
+        pass
+
+    # Showing images from the dataset in a grid
+    def revealImagesGrid(self, dataset, sampleCount, columns):
+        # Create a new figure for plotting (15 by 15) grid
+        plt.figure(figsize=(15,15))
+
+        # Loop through the entire dataset in google drive
+        for _, (image, label) in enumerate(dataset):
+            # Still not reached the end
+            if _ != sampleCount:
+                # Create subplot in the corresponding row, coln
+                rowCount = int(sampleCount/columns)
+                plt.subplot(rowCount+1, columns, _+1)
+
+                # Permute the dimensions so it displays RGB image (channels, height, width) -> (height, width, channels)
+                plt.imshow(image.permute(1,2,0))
+            else:
+                break
+        
