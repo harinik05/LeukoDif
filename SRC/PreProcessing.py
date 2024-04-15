2ca8dfe89c92b2cd6d42ded695c1fa04e20ad621
harinik05 <hka747910@gmail.com>
2024-04-15 17:24:05 -0400

    delete unnecessary function

--- SRC/PreProcessing.py
+++ SRC/PreProcessing.py
@@ -27,20 +27,5 @@ class PreProcessing():
             else:
                 break
     
-    # Showing images in a smaller grid
-    def revealSmallGrid(self, inputImage):
-        reverse_transforms = transforms.Compose([
-            transforms.Lambda(lambda t: (t + 1) / 2),
-            transforms.Lambda(lambda t: t.permute(1, 2, 0)),  # CHW to HWC
-            transforms.Lambda(lambda t: t * 255.),
-            transforms.Lambda(lambda t: t.numpy().astype(np.uint8)),
-            transforms.ToPILImage(),
-        ])
 
-        # Take first image of batch
-        if len(inputImage.shape) == 4:
-            image = image[0, :, :, :]
-
-        plt.imshow(reverse_transforms(inputImage))
-        plt.show()
         
